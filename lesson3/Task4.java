4797d5e5ac2977baca020408a4a0a4ec48d8aef9
Dmitry Sharshokov <reguldgreate@gmail.com>
2022-09-19 01:29:24 +0300

    feat: add lesson 3 Tasks 1-4

---
+++ lesson3/Task4.java
@@ -0,0 +1,39 @@
+public class Task4 {
+    public static void main(String[] args) {
+        int t =10;
+        switch (t){
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                System.out.println("Доброй ночи");
+                break;
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+                System.out.println("Доброе утро");
+                break;
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+                System.out.println("Добрый день");
+                break;
+            case 17:
+            case 18:
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+            case 23:
+                System.out.println("Добрый вечер");
+                break;
+        }
+    }
+}
